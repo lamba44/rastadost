e67fab0bead3e8f7aeab06a97502a5a0a67d376d
Saksham Lamba <sakshamlambamonu@gmail.com>
2025-03-15 10:55:21 +0530

    update: Home Page and Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 const App = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="mainbg">
-            <div className="phoneview">
-                <h1>Hello Welcome to Our App</h1>
+            <div className="phoneview home-container">
+                <h1 className="home-title">Welcome to RastaDost</h1>
+                <div className="button-group">
+                    <button
+                        className="login-button"
+                        onClick={() => navigate("/userlogin")}
+                    >
+                        User Login
+                    </button>
+                    <button
+                        className="login-button"
+                        onClick={() => navigate("/driverlogin")}
+                    >
+                        Driver Login
+                    </button>
+                </div>
             </div>
         </div>
     );
